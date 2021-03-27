2195e3eaa0aafa4d80ce50399d54604284983ac0
Azat Tagaev <braza@yandex.ru>
2021-03-28 00:24:37 +0600

    19 урок

--- script/script.js
+++ script/script.js
@@ -54,4 +54,72 @@ window.addEventListener('DOMContentLoaded', () => {
 
     countTimer('25 mar 2021');
 
+    // Меню
+    const toggleMenu = () =>  {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+    };
+
+    toggleMenu();
+
+    // PopUp
+    const togglePopUp = () => {
+
+        function animate({ timing, draw, duration }) {
+            const start = performance.now();
+
+            requestAnimationFrame(function animate(time) {
+                // timeFraction изменяется от 0 до 1
+                let timeFraction = (time - start) / duration;
+                if (timeFraction > 1) timeFraction = 1;
+
+                // вычисление текущего состояния анимации
+                const progress = timing(timeFraction);
+
+                draw(progress); // отрисовать её
+
+                if (timeFraction < 1) {
+                    requestAnimationFrame(animate);
+                }
+            });
+        }
+
+        const popUp = document.querySelector('.popup'),
+            popUpBtn = document.querySelectorAll('.popup-btn'),
+            popUpClose = document.querySelector('.popup-close');
+
+        popUpBtn.forEach(elem => {
+            elem.addEventListener('click', () => {
+                popUp.style.display = 'block';
+
+                if (screen.width > 768) {
+                    animate({
+                        duration: 500,
+                        timing(timeFraction) {
+                            return timeFraction;
+                        },
+                        draw(progress) {
+                            popUp.style.opacity = progress * 1;
+                        }
+                    });
+                }
+            });
+        });
+
+        popUpClose.addEventListener('click', () => {
+            popUp.style.display = 'none';
+        });
+    };
+
+    togglePopUp();
 });
